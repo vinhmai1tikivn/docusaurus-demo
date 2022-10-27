50e9f08f88ac43b82f01586240bbc9c1bc4ea42d
Mai Xuân Vinh <vinh.mai1@tiki.vn>
2022-10-28 00:55:27 +0700

    feat: clone and init repo

---
+++ docusaurus.config.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+"use strict";
+
+const siteConfig = {
+  title: "Create Console App",
+  tagline: "Set up a modern web app by running one command.",
+  url: "https://create-react-app.dev",
+  baseUrl: "/",
+  projectName: "create-react-app",
+  organizationName: "facebook",
+  favicon: "img/favicon/favicon.ico",
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          path: "./docs",
+          sidebarPath: require.resolve("./sidebars.json"),
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+  themeConfig: {
+    image: "img/logo-og.png",
+    navbar: {
+      title: "Tiki Console Docs",
+      logo: {
+        alt: "Tiki Console Docs Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        { to: "docs/getting-started", label: "Docs", position: "right" },
+        {
+          href: "https://console.tala.xyz/apps",
+          label: "Console",
+          position: "right",
+        },
+      ],
+    },
+  },
+};
+
+module.exports = siteConfig;
