258d081835c32d5b532d030ac71c7052c93f9bf8
vinhmai1tikivn <105188484+vinhmai1tikivn@users.noreply.github.com>
2022-10-28 01:25:04 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const siteConfig = {
   title: "Create Console App",
   tagline: "Set up a modern web app by running one command.",
   url: "https://create-react-app.dev",
-  baseUrl: "https://vinhmai1tikivn.github.io/docusaurus-demo/",
+  baseUrl: "/",
   projectName: "create-react-app",
   organizationName: "facebook",
   favicon: "img/favicon/favicon.ico",
